8a32affa6bc8f1abfe3c3f45b640dee0faafdf39
Ben Konrad Meyer <ben.k.meyer@gmail.com>
2020-05-21 20:28:00 +0200

    added input evaluation. TO DO: training

--- Programms/R/myNN.R
+++ Programms/R/myNN.R
@@ -1,1 +1,17 @@
-weights <- array(data = runif(26*26), dim = c(10,26*26))
+# inputsize = 784 (28*28)
+
+weights <- array(data = runif(784*10), dim = c(10,784))
+
+evaluate <- function(input) {
+  if(!is.matrix(input)) stop("input must be a matrix")
+  if(length(input) != 784) stop("input must have a length of 784")
+  output <- weights %*% input
+  return(output)
+}
+
+classify <- function(input) {
+  if(!is.matrix(input)) stop("input must be a matrix")
+  if(length(input) != 784) stop("input must have a length of 784")
+  result <- evaluate(input)
+  return(which.max(result))
+}
