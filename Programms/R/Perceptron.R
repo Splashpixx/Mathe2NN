48a32ba6baa9567c9a711659fa16abf41a3ba171
Ben Konrad Meyer <ben.k.meyer@gmail.com>
2020-05-25 09:26:23 +0200

    added Delta-rule to perceptron

--- Programms/R/Perceptron.R
+++ Programms/R/Perceptron.R
@@ -1,12 +1,12 @@
-# inputsize = 784 (28*28)
-Perceptron <- function(pInputSize = 784, pOutputSize = 10){
+Perceptron <- function(pInputSize = 784, pOutputSize = 10, learningfactor = 0.01){
   envi <- environment()
+  
   # data fields (private)
   inputSize <- pInputSize
   outputSize <- pOutputSize
-  weights <- array(data = runif(inputSize * outputSize), dim = c(outputSize, inputSize))
+  weights <- array(data = runif(inputSize * outputSize, min = -1), dim = c(outputSize, inputSize))
   
-  # functions
+  # functions (the equals sign is mandatory)
   me <- list(
     envi = envi,
     
@@ -35,7 +35,7 @@ Perceptron <- function(pInputSize = 784, pOutputSize = 10){
     
     evaluate = function(input) {
       if(!is.matrix(input)) stop("input must be a matrix")
-      if(!identical( dim(input) , as.integer(c(inputSize, 1)))) 
+      if(!identical( dim(input) , as.integer(c(inputSize)))) 
         stop(cat("Dimension mismatch in evaluate! Dimension of input needs to be", inputSize, "x 1"))
       output <- weights %*% input
       return(output)
@@ -43,17 +43,37 @@ Perceptron <- function(pInputSize = 784, pOutputSize = 10){
     
     classify = function(input) {
       if(!is.matrix(input)) stop("input must be a matrix")
-      if(!identical( dim(input) , as.integer(c(inputSize, 1)))) 
+      if(!identical( dim(input) , as.integer(c(inputSize)))) 
         stop(cat("Dimension mismatch in classify! Dimension of input needs to be", inputSize, "x 1"))
       result <- me$evaluate(input)
       return(which.max(result))
+    },
+    
+    train = function(input, output) {
+      if(!is.matrix(inputData)) stop("inputData must be a matrix")
+      if(!is.matrix(outputData)) stop("outputData must be a matrix")
+      if(!identical( dim(inputData) , as.integer(c(inputSize)))) 
+        stop(cat("Dimension mismatch in classify! Dimension of inputData needs to be", inputSize, "x 1"))
+      if(!identical( dim(outputData) , as.integer(c(outputSize)))) 
+        stop(cat("Dimension mismatch in classify! Dimension of inputData needs to be", inputSize, "x 1"))
+      result <- me$evaluate(inputData)
+      
+      # Deltaregel
+      gradient <- array(dim = c(outputSize, inputSize))
+      for(outputNeuron in 1:nrow(gradient)){
+        for(inputNeuron in 1:ncol(gradient)){
+          # \Delta w_ik = e * d_i * a_k
+          # e is out of loop
+          d <- output[outputNeuron] - result[outputNeuron]
+          gradient[outputNeuron, inputNeuron] <- d * input[inputNeuron]
+        }
+      }
+      weights <- weights + learnfactor * gradient
     }
+    
   )
     
   assign('this', me, envir = envi)
   class(me) <- append(class(me), 'Perceptron')
   return(me)
 }
-
-
-
